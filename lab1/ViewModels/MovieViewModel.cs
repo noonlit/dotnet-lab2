8daa9c2fbd92ae5192d904f83b3fa76e799fc950
noonlit <julietfeynman@gmail.com>
2021-05-08 15:31:16 +0300

    Update MovieViewModel.cs

--- lab1/ViewModels/MovieViewModel.cs
+++ lab1/ViewModels/MovieViewModel.cs
@@ -3,40 +3,30 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Lab1.ViewModels
 {
-    public class MovieViewModel
-    {
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-
-namespace Lab1.Models
+	public class MovieViewModel
 	{
-		public class Movie
-		{
 			public enum GenreType
-			{
-				Action, Comedy, Horror, Thriller
-			}
-
-			public int Id { get; set; }
-
-			public string Title { get; set; }
-
-			public string Description { get; set; }
-
-			public GenreType Genre { get; set; }
-
-			public float DurationMinutes { get; set; }
-
-			public int ReleaseYear { get; set; }
-
-			public string Director { get; set; }
-
-			public DateTime AddedAt { get; set; }
-			public int Rating { get; set; }
-
-			public bool Watched { get; set; } = false;
+		{
+			Action, Comedy, Horror, Thriller
 		}
-	}
 
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public GenreType Genre { get; set; }
+
+		public float DurationMinutes { get; set; }
+
+		public int ReleaseYear { get; set; }
+
+		public string Director { get; set; }
+
+		public DateTime AddedAt { get; set; }
+		public int Rating { get; set; }
+
+		public bool Watched { get; set; } = false;
+	}
 }
