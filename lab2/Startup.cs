8418d179425a86b14c9183c3ed9974fe6cda93c0
noonlit <julietfeynman@gmail.com>
2021-05-14 19:48:41 +0300

    Remove swagger and unused dependencies

--- lab2/Startup.cs
+++ lab2/Startup.cs
@@ -11,12 +11,7 @@ using Lab2.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using System;
-using System.Reflection;
-using System.IO;
-using AutoMapper;
-using Lab2.ViewModels;
 
 namespace Lab2
 {
@@ -61,39 +56,12 @@ namespace Lab2
 							});
 
 			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo
-				{
-					Version = "v1",
-					Title = "ToDo API",
-					Description = "A simple example ASP.NET Core Web API",
-					TermsOfService = new Uri("https://example.com/terms"),
-					Contact = new OpenApiContact
-					{
-						Name = "Shayne Boyer",
-						Email = string.Empty,
-						Url = new Uri("https://twitter.com/spboyer"),
-					},
-					License = new OpenApiLicense
-					{
-						Name = "Use under LICX",
-						Url = new Uri("https://example.com/license"),
-					}
-				});
-			});
-
 			services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			app.UseSwagger();
-			app.UseSwaggerUI(c =>
-			{
-				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-			});
-
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
